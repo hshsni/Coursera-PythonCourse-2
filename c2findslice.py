1496d3d3d7074c288a5494115f5e271382c1a9cd
hshsni <71019446+hshsni@users.noreply.github.com>
2021-07-10 19:42:51 +0530

    Add files via upload

---
+++ c2findslice.py
@@ -0,0 +1,5 @@
+#Write code using find() and string slicing to extract the number at the end of the line below.
+#Convert the extracted value to a floating point number and print it out.
+text = "X-DSPAM-Confidence:    0.8475"
+pos=text.find('0')
+print(float(text[pos:]))
